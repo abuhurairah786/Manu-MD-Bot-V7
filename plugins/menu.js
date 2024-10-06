9fc39640b5099dda61b55b565b4023aaad02b6cb
abuhurairah786 <103510868+abuhurairah786@users.noreply.github.com>
2024-10-06 20:09:02 +0500

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -20,19 +20,19 @@
 
 //══════════════════════════════════════════════════════════════════════════════════════════════════════//
 //*
-//  * @project_name : MANU-MD
-//  * @author : Manul Official
-//  * @youtube : https://www.youtube.com/@ManulOfficialTech
-//  * @description : Manu-MD ,A Multi-functional whatsapp user bot.
+//  * @project_name : 🅗🅤🅡🅐🅘🅡🅐🅗-🅜🅓
+//  * @author : ABUHURAIRAH
+//  * @youtube : https://www.youtube.com/@HURAIRAH
+//  * @description : 🅗🅤🅡🅐🅘🅡🅐🅗-🅜🅓 ,A Multi-functional whatsapp user bot.
 //*
 //*
-//base by Manul Official
-//GitHub: @manulofficial7
-//WhatsApp: +94742274855
+//base by Abuhurairah 
+//GitHub: @HURAIRAH
+//WhatsApp: +923449412639
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@ManulOfficialTech
-//   * Created By Github: manulofficial7.
-//   * Credit To Manul Official
-//   * © 2024 MANU-MD-V7.
+//   * Created By Github: abuhurairah786.
+//   * Credit To 🇭 🇺 🇷 🇦 🇮 🇷 🇦 🇭 -🇲 🇩 
+//   * © 2024 ▄︻デH̷U̷R̷A̷I̷R̷A̷H̷-̷M̷D̷══━一.
 // ⛥┌┤
 // */
 
